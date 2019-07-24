12926347fbd64ab2188052add11f61362a752ed1
t3kj3wlz <outlawstar4761@gmail.com>
2019-07-24 16:43:35 -0500

    unpackMethod error catching

--- Archive.php
+++ Archive.php
@@ -3,4 +3,15 @@
 class Archive{
   public static $archiveTypes = array('rar','zip','7z');
   public $unpackMethods = array('unp','unzip','p7zip -d');
+
+  public function __construct(){}
+
+  public static function extract($absolutePath, $destination = ""){
+    $unpackIndex = array_search(pathinfo($absolutePath)['extension'],self::$archiveTypes);
+    if($unpackIndex === false){
+      throw new \Exception('No Support For ' . pathinfo($absolutePath)['extension']);
+    }else{
+      $unpackMethod = self::$unpackMethods[$unpackIndex];
+    }
+  }
 }
